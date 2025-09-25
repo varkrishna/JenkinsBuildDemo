6670334f3e2b03956b9ee5b18e45566eeec72398
varkrishna <krishan.varshney708@gmail.com>
2025-09-25 23:58:26 +0530

    Initial commit

---
+++ SimpleAppJenkins/SimpleAppJenkins/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleAppJenkins
+//
+//  Created by krishna varshney on 25/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleAppJenkins/SimpleAppJenkins/SimpleAppJenkinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppJenkinsApp.swift
+//  SimpleAppJenkins
+//
+//  Created by krishna varshney on 25/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppJenkinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
